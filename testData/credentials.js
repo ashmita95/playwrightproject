7e074bccf688bd5b7e5909ea38959a91af2b6408
ashmita95 <ashmita.sur@gmail.com>
2026-04-07 10:32:03 +0530

    loggedin

---
+++ testData/credentials.js
@@ -0,0 +1,14 @@
+export const credentials = {
+    qa: {
+      //url: 'https://bsl.qa.clockworkrecruiting.dev',
+      accountUrl: 'https://account.qa.clockworkrecruiting.dev/accounts',
+      email: 'ashmita+cypress@clockworkrecruiting.com',
+      password: 'KREETI@12345',
+      firmname: 'AIW (AI Workspace)',
+    },
+
+    demo: {
+     
+    },
+
+};
